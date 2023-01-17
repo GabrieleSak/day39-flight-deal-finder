4873cfcf90ba09b69562160a10bc5d4f86935a0b
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-01-17 09:40:06 +0200

    Get flight data from Google Sheet using Sheety API

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,19 @@
+import requests as requests
+from auth import sheet_username, projectName, sheetName, sheet_token
+
+SHEET_USERNAME = sheet_username
+PROJECT_NAME = projectName
+SHEET_NAME = sheetName
+SHEET_TOKEN = sheet_token
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self):
+        self.sheet_endpoint = f"https://api.sheety.co/{SHEET_USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
+        self.sheet_headers = {"Authorization": f"Bearer {SHEET_TOKEN}"}
+
+    def get_flight_data(self):
+        sheet_response = requests.get(self.sheet_endpoint, headers=self.sheet_headers)
+        return sheet_response.json()
+
